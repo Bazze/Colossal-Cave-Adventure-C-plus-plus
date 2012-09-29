7485276288e05eb7805d3016cfc3a557eb0e9424
Sebastian Norling <norling.sebastian@gmail.com>
2012-09-29 13:43:04 +0200

    Initial Commit

---
+++ Colossal Cave Adventure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Colossal Cave Adventure
+//
+//  Created by Sebastian Norling on 2012-09-29.
+//  Copyright (c) 2012 SN Solutions. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
