3c6387ec17bfda093387dfa4972b380d099725d2
Niclas Björner <niclas.bjorner@gmail.com>
2012-10-04 13:46:45 +0200

    Added code to Server.cpp
    The code should handle the incoming TCP requests. Will comment it a little more later.

--- Colossal Cave Adventure/Server.cpp
+++ Colossal Cave Adventure/Server.cpp
@@ -10,3 +10,31 @@
 #include "PracticalSocket.h" // For Socket, ServerSocket and SocketException
 #include <iostream>
 #include <cstdlib>
+
+const unsigned int RECEIVEBUFFERSIZE = 128;
+
+void HandleTCPClient(TCPSocket *socket);
+
+//Handling for TCP-Client
+void HandleTCPClient(TCPSocket *socket) {
+    cout << "Client handling ";
+    try {
+        cout << socket->getForeignAddress() << ":";
+    } catch (SocketException e) { 
+        cerr << "Network: Unable to get foreign address" << endl;
+    }
+    try {
+        cout << socket->getForeignPort();
+    } catch (SocketException e) {
+        cerr << "Network: Unable to get foreign port" << endl;
+    }
+    
+    char buffer[RECEIVEBUFFERSIZE];
+    int receivMessageSize;
+    while ((receivMessageSize = socket->recv(buffer, RECEIVEBUFFERSIZE)) > 0) {
+        //send the message for parsing
+        
+        //get message to send back
+    }
+        delete socket;
+}
